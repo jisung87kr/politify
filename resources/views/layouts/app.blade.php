2a5634ed8c65d1eb17a225161ba2a6524f2aca47
jisungyoo <jisung87kr@gmail.com>
2024-12-09 23:19:17 +0900

    다듬기

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                 {{ $slot }}
             </main>
             <footer class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div class="py-6">footer</div>
+                <div class="py-6"><span>@</span>{{date('Y')}} politify.co.kr</div>
             </footer>
         </div>
 
