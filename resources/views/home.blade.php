f8f0f2dac2eec3551e34cfd8a156d486a5539960
jisungyoo <jisung87kr@gmail.com>
2024-12-15 16:00:48 +0900

    대표 발의안 추가

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,7 +79,6 @@
                         @if($member->last_end >= 10)
                         <div class="flex mt-4 md:mt-6">
                             <a href="{{ route('member.show', $member) }}"
-                               target="_blank"
                                class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">자세히보기</a>
                         </div>
                         @endif
